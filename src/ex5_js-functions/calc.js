add0569b9e140d27c19f289e262ca608e7ba30b7
Gav0r <ghavrikov.sasha@mail.ru>
2021-10-19 00:03:41 +0300

    master commit ex-5

---
+++ src/ex5_js-functions/calc.js
@@ -0,0 +1,71 @@
+function Calculator() {
+  let value = 0;
+
+  return {
+    /**
+     * @param {number} incrementValue
+     */
+    add(incrementValue = 0) {
+      if (typeof incrementValue !== 'number') throw new Error('add accepts only a number');
+      value += incrementValue;
+      return function innerAdd(subIncrementValue = 0) {
+        if (typeof subIncrementValue !== 'number') throw new Error('add accepts only a number');
+        value += subIncrementValue;
+        return innerAdd;
+      };
+    },
+    /**
+     * @param {number} decrementValue
+     */
+    subtract(decrementValue = 0) {
+      if (typeof decrementValue !== 'number') throw new Error('subtract accepts only a number');
+
+      value -= decrementValue;
+      return function innerSubstract(subDecrementValue = 0) {
+        if (typeof subDecrementValue !== 'number') throw new Error('subtract accepts only a number');
+        value -= subDecrementValue;
+        // return this.subtract; - моё
+        return innerSubstract;
+      };
+    },
+    /**
+     * @param {number} divideValue
+     */
+    divide(divideValue = 0) {
+      if (typeof divideValue !== 'number') throw new Error('divide accepts only a number');
+      if (divideValue !== 0) {
+        value /= divideValue;
+      }
+      return function innerDivide(subDivideValue = 0) {
+        if (typeof subDivideValue !== 'number') throw new Error('divide accepts only a number');
+        value /= subDivideValue;
+        return innerDivide;
+      };
+    },
+    /**
+     * @param {number} multiplyValue
+     */
+    multiply(multiplyValue = 0) {
+      if (typeof divideValue !== 'number') throw new Error('multiply accepts only a number');
+      console.log('multiply call');
+      if (multiplyValue !== 0) {
+        value *= multiplyValue;
+      }
+      return function innerMultiply(subMultiplyValue = 0) {
+        if (typeof subDivideValue !== 'number') throw new Error('multiply accepts only a number');
+        value *= subMultiplyValue;
+        return innerMultiply;
+      };
+    },
+    getResult() {
+      return value;
+    },
+    reset() {
+      value = 0;
+    },
+  };
+}
+
+const calculator = Calculator();
+
+module.exports = calculator;
